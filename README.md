a9f0cc8f9e7cbc5567af5b70bae4a7f676b2c56a
ggendro <g.gendron35@gmail.com>
2024-04-05 02:27:14 +0000

    added loading from config file

--- README.md
+++ README.md
@@ -4,15 +4,49 @@ Repository of the Causal Bayesian Neural Network (CBNN).
 
 
 # Installation
+
+Run the following command to install the required packages:
 ```bash
 pip install -r requirements.txt
 ```
 
-# Usage
+You can install the packages in a virtual environment by running the following commands:
 ```bash
-python run.py
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 ```
 
+
+# Usage
+
+To run the experiments from a config file, use the following command: 
+```bash
+python run.py --load_config 'config/<config_file>.yaml' '<model_name>'
+```
+
+You can also override any parameter from the config file by passing it as an argument:
+```bash
+python run.py --load_config 'config/<config_file>.yaml' --<parameter> '<value>' '<model_name>' --<model_parameter> '<value>'
+```
+
+Use the `-h` flag to see the available options:
+```bash
+python run.py -h
+```
+
+To see the model options, use the following command:
+```bash
+python run.py '<model_name>' -h
+```
+
+Common arguments:
+- `--load_config`: Load the configuration file.
+- `--save_config`: Save the configuration file.
+- `--data`: Dataset to use.
+- `--save`: Path to save the model.
+- `--train`, `--test`, `--train_and_test`: Train, test, or train and test the model. Default option is `train_and_test`.
+- `--max_epochs`: Maximum number of epochs. You might want to set it as the default value of Pytorch Lightning is 1000.
 
 
 ## Architecture
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 
 import argparse
+import yaml
 
 from src.cbnn.data.datasets import DATASETS
 from src.cbnn.model.models import get_model, add_model_specific_args
@@ -10,19 +11,40 @@ from pytorch_lightning.loggers import WandbLogger
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train and test a model or load existing model for inference.')
+
+    # Add arguments
     parser.add_argument('--data', type=str, default='MNIST', help=f'Dataset to use for training. Options: {", ".join(DATASETS.keys())}')
     parser.add_argument('--save', type=str, default=None, help='Path to saved model to load for inference. If None, train a new model from scratch.')
     parser.add_argument('--wandb_project', type=str, default=None, help='If specified, logs the run to wandb under the specified project.')
+    parser.add_argument('--load_config', type=str, default=None, help='Path to a yaml config file to load arguments from. Arugments provided in the command line will override the config file.')
+    parser.add_argument('--save_config', type=str, default=None, help='Path to save the current arguments to a config file.')
 
+    # Add mutually exclusive group for training, testing, or both
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--train', action='store_true', help='Only train the model.')
     group.add_argument('--test', action='store_true', help='Only test the model.')
     group.add_argument('--train_and_test', action='store_true', help='Train a new model and test it. Default option.')
 
+    # Add PyTorch Lightning Trainer arguments an model specific arguments
     parser = pl.Trainer.add_argparse_args(parser)
     parser = add_model_specific_args(parser)
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    # If load_config is specified, load the config file and update the arguments
+    if args.load_config is not None:
+        with open(args.load_config, 'r') as f:
+            config = yaml.safe_load(f)
+            args = argparse.Namespace(**config, **vars(args))
+
+    # If save_config is specified, save the current arguments to a config file
+    if args.save_config is not None:
+        args_dict = vars(args)
+        save_file = args_dict.pop('save_config') # do not keep the save_config argument in the saved config file to avoid overriding it when loading
+        with open(save_file, 'w') as f:
+            yaml.dump(args_dict, f)
+
+    return args
 
 
 
